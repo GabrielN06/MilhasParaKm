bb296abfbcb80259163327dc08bfacb2713fe29f
Gabriel <gabrielnunesdacostaanselmo@gmail.com>
2025-03-24 08:33:08 -0300

    Programa Finalizado

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿double distancia;
+
+Console.Write("Entre com a medida (em milhas): ");
+distancia = Convert.ToDouble(Console.ReadLine());
+
+distancia *= 1609;
+
+Console.WriteLine($"Em Kms seria: {distancia}");
